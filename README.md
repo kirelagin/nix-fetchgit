0d35bac30020f56006d18bf0195310e80cf328ae
Kirill Elagin <kirelagin@gmail.com>
2019-01-16 19:31:24 +0300

    Initial version

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# Nix shelter
+# Nix fetchGit with `ref = "*"`
 
-_Good old `nix-shell` but without the dash and feels like home._
+This repository is two things:
+
+* A Nix plugin that replaced `fetchGit` with the one from [NixOS/nix#2409][patch].
+* A function which you can use to wrap your Nix and have this plugin activated.
+
+It is essentially the same as applying the patch from the pull request, but builds faster.
+
+
+  [patch]: https://github.com/NixOS/nix/pull/2409
---
+++ default.nix
@@ -0,0 +1,51 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+origNix:
+
+with pkgs;
+
+let
+  nix-fetchgit =
+    stdenv.mkDerivation {
+      name = "nix-fetchgit";
+
+      src = lib.sourceByRegex ./. ["^CMakeLists.txt$"];
+      patchSrc = fetchpatch {
+        url = "https://github.com/NixOS/nix/commit/4b279a099f19bd13a98fa2ae519c4aa263bb6b6e.patch";
+        sha256 = "0sisavs3x2p37hrmhnhvhh2rzb0qyjvvd4y0azk25329wylqnw88";
+      };
+
+      postPatch = ''
+        tar -xf "${nix.src}" --strip-components 4 "${nix.name}"/src/libexpr/primops/fetchGit.cc
+        tar -xf "${nix.src}" --strip-components 2 "${nix.name}"/src/nlohmann
+        patch -p4 < "$patchSrc"
+      '';
+
+      nativeBuildInputs = [ cmake pkgconfig ];
+      buildInputs = [ origNix boost ];
+
+      meta = {
+        # https://github.com/NixOS/nix/pull/2409
+        description = ''A fetchGit replacement plugin which supports ref = "*"'';
+        homepage = https://github.com/kirelagin/nix-fetchgit;
+        license = stdenv.lib.licenses.mit;
+        platforms = stdenv.lib.platforms.all;
+      };
+    };
+
+in
+
+symlinkJoin {
+  inherit (origNix) name;
+
+  paths = [ origNix ];
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  postBuild = ''
+    for p in "$out"/bin/*; do
+      wrapProgram "$p" \
+        --add-flags "--option plugin-files ${nix-fetchgit}/lib/nix/plugins/libnix-fetchgit*"
+    done
+    '';
+}
